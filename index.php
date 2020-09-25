33891d55a51420aca721ffdda07104eeb2cabbac
timonen <timone_n@outlook.com>
2020-09-25 13:51:50 +0300

    Update

--- index.php
+++ index.php
@@ -1,20 +1,42 @@
 <?php
 
-include 'sys/variables.php';
-include 'sys/classes.php';
-include 'sys/functions.php';
+include __DIR__.'/sys/variables.php';
+include __DIR__.'/sys/classes.php';
+include __DIR__.'/sys/functions.php';
+
+$page = new Page("Main", "", $PAGE_FRONT);
+
+if(isset($_GET['err']))
+	switch ($_GET['err']) {
+		case '400':
+			$page->update("Server Error", "", $ERROR_400);
+			break;
+
+		case '500':
+			$page->update("Server Error", "", $ERROR_500);
+			break;
+	}
+
+if(isset($_GET['page']))
+	switch ($_GET['page']) {
+		case 'second':
+			$page->update("Second", "", $PAGE_SECOND);
+			break;
+
+		case 'test':
+			$page->update("Test", "", $PAGE_TEST);
+			break;
+
+		default:
+			$page->update("ERROR", "", $PAGE_TEST);
+			break;
+	}
 
 
-$test = new Page("Main", "", $PAGE_FRONT);
-
-if(isset($_GET['test'])){
-	$test->update("Second", "", $PAGE_SECOND);
-}
 
 
 
 
-
-$test->render();
+$page->render();
 
 ?>
