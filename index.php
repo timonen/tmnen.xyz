ff85141b80f9e8403b511285649899ecea0508c9
timonen <timone_n@outlook.com>
2020-09-26 16:32:56 +0300

    Update

--- index.php
+++ index.php
@@ -6,39 +6,56 @@ include __DIR__.'/sys/functions.php';
 
 session_start();
 
-$page = new Page("Main", $PAGE_FRONT);
+$page = new Page(array("title"=>"Main", "content"=>$PAGE_FRONT));
 
 if(isset($_GET['err']))
 	switch ($_GET['err']) {
 		case '400':
-			$page->update("Server Error", $ERROR_400);
+			$page->update(array("title"=>"ERROR", "content"=>$ERROR_400));
 			break;
 
 		case '500':
-			$page->update("Server Error", $ERROR_500);
+			$page->update(array("title"=>"Server Error", "content"=>$ERROR_500));
 			break;
 	}
 
 if(isset($_GET['page']))
 	switch ($_GET['page']) {
 		case 'second':
-			$page->update("Second", $PAGE_SECOND);
+			$page->update(array("title"=>"Second", "content"=>$PAGE_SECOND));
 			break;
 
 		case 'test':
-			$page->update("Test", $PAGE_TEST);
+			$page->update(array("title"=>"TEST", "content"=>$PAGE_TEST));
+			break;
+
+		case 'debug':
+			$page->update(array("body"=>1, "title"=>"TEST", "content"=>"REEEE"));
 			break;
 
 		default:
-			$page->update("ERROR", $PAGE_TEST);
+			$page->update(array("title"=>"ERROR", "content"=>$ERROR_400));
+			break;
+	}
+
+if(isset($_GET['file']))
+	switch ($_GET['file']) {
+		case 'image':
+			$page->update(array("title"=>"Image", "content"=>'<h1>'.$_GET['fn'].'<h1>'));
+			break;
+		
+		case 'vid':
+			$page->update(array("title"=>"Video", "content"=>'<h1>'.$_GET['fn'].'<h1>'));
+			break;		
+
+		default:
+			$page->update(array("title"=>"ERROR", "content"=>$ERROR_400));
 			break;
 	}
 
 
 
 
-
-
-$page->render();
+$page->send();
 
 ?>
--- sys/classes.php
+++ sys/classes.php
@@ -1,49 +1,69 @@
 <?php
 
 class Page {
-	private $title;
-	private $content;
-
-	function __construct($t, $cn){
-		$this->title = $t;
-		$this->content = $cn;
+	function __construct($arr){
+		$this->title = $arr['title'];
+		$this->content = $arr['content'];
+		$this->css = $arr['css'];
+		$this->sjs = $arr['ejs'];
+		$this->ejs = $arr['ejs'];
+		$this->u_body($arr['body']);
 	}
 
-	function update($t, $cn){
-		$this->title = $t;
-		$this->content = $cn;
+	function update($arr){
+		$this->title = $arr['title'];
+		$this->content = $arr['content'];
+		$this->css = $arr['css'];
+		$this->sjs = $arr['ejs'];
+		$this->ejs = $arr['ejs'];
+		$this->u_body($arr['body']);
 	}
 
-	function render(){
-		echo '<!doctype html>
-			<html lang="Fi">
-			<head>
-				<meta charset="utf-8">
-				<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-				<title>',$this->title,'</title>
-				<meta name="description" content="Startpage">
-			    <meta name=viewport content="width=device-width, initial-scale=.75">
-				<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
-				<link rel="stylesheet" href="../css/def.css">
-				',$this->css,'
-				<script src="../js/functions.js"></script>
-			</head>
-			<body>
-			
-			<div id="nav"></div>
-			<canvas id="bg"></canvas>
-			<div id="inside">
-			
-				', $this->content ,'
-					
-				<div id="footer">
-					<p>',$this->title,' | tmnen.xyz</p>
+	function u_body($id){
+		if(isset($id)){
+
+			$this->body = "<h1>".$this->content."</h1>";
+
+		}else{
+			$this->body = '<!doctype html>
+				<html lang="Fi">
+				<head>
+					<meta charset="utf-8">
+					<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+					<meta name="description" content="spagetti">
+				    <meta name=viewport content="width=device-width, initial-scale=.75">
+	
+						<title>'.$this->title.'</title>
+	
+					<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+					<link rel="stylesheet" href="../css/def.css">
+					'.$this->css.'
+	
+					<script src="../js/functions.js"></script>
+					'.$this->sjs.'
+				</head>
+				<body>
+				
+				<div id="nav"></div>
+				<canvas id="bg"></canvas>
+				<div id="inside">
+	
+					'. $this->content .'
+	
+					<div id="footer">
+						<p>'.$this->title.' | tmnen.xyz</p>
+					</div>
 				</div>
-			</div>
-				<script src="../js/main.js"></script>
-				<script async src="../js/draw.js"></script>
-			</body>
+					<script src="../js/main.js"></script>
+					'.$this->ejs.'
+					<script async src="../js/draw.js"></script>
+				</body>
 			</html>';
+		}
+	}
+
+	function send(){
+		echo $this->body;
 	}
 }
 
