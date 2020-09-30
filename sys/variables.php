f0d46b3a225ee886d61ca42cb2aaf1d4101c95c7
timonen <timone_n@outlook.com>
2020-09-30 20:32:11 +0300

    Update

--- sys/variables.php
+++ sys/variables.php
@@ -6,7 +6,7 @@ $PAGE_SECOND = "<h1>Second Page!</h1>";
 $ERROR_400 = "<h1>Error 404: Page not found!</h1>"; 
 $ERROR_500 = "<h1>Server Error!</h1>";
 
-$PAGE_UPLOAD = "<form action='/?file&up' method='post' enctype='multipart/form-data'>
+$PAGE_UPLOAD = "<form id='upld' action='/?file&up' method='post' enctype='multipart/form-data'>
 	<br><br><input type='file' name='file'/><br>
 	<input type='submit' name='submit' value='Upload'/>
 </form>";
