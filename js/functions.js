e190c42ee0f4d0123e3efb1f835bfe8f9eb30232
Roope Timonen <timone_n@outlook.com>
2020-10-01 13:23:26 +0300

    syntax cleaning

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,10 @@
 var log = (txt, lvl) => {
 		switch(lvl){
 			case 'alert':
-				console.warn('\n '+txt+'\n ');
+				console.warn(txt);
 				break;
 			case 'err':
-				console.error('\n '+txt+'\n ');
+				console.error(txt);
 				break;
 			default:
 				console.log(txt);
@@ -15,7 +15,7 @@ var log = (txt, lvl) => {
 	evt = (id, e, f) => id.addEventListener(e, f, true);
 
 class Windw {
-	constructor(e, w, h){
+	constructor(e, w, h) {
 		this.e = e;
 		this.e.width = w;
 		this.e.height = h;
@@ -26,12 +26,12 @@ class Windw {
 			requestAnimationFrame(this.loop);
 		};
 	}
-	update(w, h){
+	update(w, h) {
 		this.e.width = w;
 		this.e.height = h;
 		this.c = this.e.getContext('2d');
 	}
-	tick(f){
+	tick(f) {
 		this.run = f;
 	}
 }
